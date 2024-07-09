156e35acf0271632abcbdc325e9253a33d9e8036
techieSimran <142678983+SimranRangrez@users.noreply.github.com>
2024-07-09 23:01:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = " Cheers to another year!!!";
+
+desc.innerText = " Happy Birthday to the most amazing, funny, and sometimes infuriating person I know! (But hey, wouldn't have it any other way.) Here's to another year of making memories, sharing snacks, and laughing until our sides ache.";
+
+sname.innerText = "MY LOVE";
